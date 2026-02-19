b3720bde1de1a33346ce4a00605ec6d1c35e3101
Derya Kımırtı <96728812+deryakmrt@users.noreply.github.com>
2026-02-19 13:59:47 +0300

    update migrations

---
+++ src/ErpSystem.Infrastructure/Migrations/20260219085411_AddProductAttributesTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ErpSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductAttributesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "productattributes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Options = table.Column<string>(type: "text", nullable: false),
+                    SystemKey = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_productattributes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "productattributes");
+        }
+    }
+}
